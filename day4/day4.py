39bf396cf9a0411cccd246fb6884640151d1160f
fernando24164 <fernando24164@gmail.com>
2017-12-10 20:16:36 +0100

    Add day4 part 2 solution

--- day4/day4.py
+++ day4/day4.py
@@ -8,3 +8,17 @@ with open('data.txt', 'r') as input_data:
             valid_passphrase += 1
 
 print(valid_passphrase)
+
+# part 2
+valid_passphrase = 0
+with open('data.txt', 'r') as input_data:
+    for line in input_data:
+        words=line.replace('\n','').split(' ')
+
+        for index, element in enumerate(words):
+            words[index] = ''.join(sorted(element))
+
+        if(len(words) == len(set(words))):
+            valid_passphrase += 1
+
+print(valid_passphrase)
